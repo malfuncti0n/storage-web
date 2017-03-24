74dfae15e896dda0908463911b2f997f77403058
malfuncti0n <savvaniss@yahoo.gr>
2017-03-24 23:05:06 +0200

    implement toggle in navigation for mobile devices

--- src/app/typescripts/navbar.component.ts
+++ src/app/typescripts/navbar.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
     selector: 'custom-navbar',
     templateUrl: '../templates/navbar.component.html'
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+    //function to check collapse state for navigation menu and togle collapse
+    isIn = false; //default state false
+    toggleState() {
+        let bool = this.isIn;
+        this.isIn = bool === false ? true : false;
+
+    }
+
+}
