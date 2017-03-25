63c206a8c95f88cb658f22402ba273df2c114870
malfuncti0n <savvaniss@yahoo.gr>
2017-03-25 16:22:31 +0200

    some 2 way binding in register form

--- src/app/typescripts/register.component.ts
+++ src/app/typescripts/register.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../user.register.model';
 
 @Component({
     templateUrl: '../templates/register.component.html'
 })
-export class RegisterComponent { }
+export class RegisterComponent {
+submitted = false;
+model = new User('emai','username','firstname','last','password');
+onSubmit() { this.submitted = true; }
+
+ // TODO: Remove this when we're done
+get diagnostic() { return JSON.stringify(this.model); }
+}
---
+++ src/app/user.register.model.ts
@@ -0,0 +1,10 @@
+    export class User {
+      constructor(
+        public email: string,
+        public username: string,
+        public password: string,
+        public firstname?: string,
+        public lastname?: string,
+
+      ) {  }
+    }
